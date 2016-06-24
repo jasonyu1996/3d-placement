d167a5dabb1ae37c308637cb8eb66f2e89e29d49
cyvius96 <cyvius96@gmail.com>
2016-06-24 23:34:52 +0800

    add naive & kdtree algorithm for contourstructure

---
+++ ContourStructure/kdtreecontourstructure.h
@@ -0,0 +1,34 @@
+#ifndef KDTREECONTOURSTRUCTURE_H
+#define KDTREECONTOURSTRUCTURE_H
+
+#include "contourstructure.h"
+#include <cstddef>
+
+class KDTreeContourStructure : public ContourStructure {
+public:
+	KDTreeContourStructure(int n=0, int m=0);
+	~KDTreeContourStructure();
+	int Update(int x1, int y1, int x2, int y2, int add);
+private:
+	struct KDNode {
+		int x1, x2, y1, y2;
+		bool dtype;
+		int tag;
+		int maxv;
+		KDNode *ls;
+		KDNode *rs;
+		KDNode(int x1, int x2, int y1, int y2, bool dtype):
+			x1(x1), x2(x2), y1(y1), y2(y2), dtype(dtype) {
+				tag = -1;
+				maxv = 0;
+				ls = NULL;
+				rs = NULL;
+			}
+	} *root;
+	void KDPush(KDNode *p);
+	int KDQuery(KDNode *p, int x1, int y1, int x2, int y2);
+	void KDModify(KDNode *p, int x1, int y1, int x2, int y2, int val);
+	void KDDestory(KDNode *p);
+};
+
+#endif // KDTREECONTOURSTRUCTURE_H
--- ContourStructure/naivecontourstructure.h
+++ ContourStructure/naivecontourstructure.h
@@ -1,12 +1,16 @@
 #ifndef NAIVECONTOURSTRUCTURE_H
 #define NAIVECONTOURSTRUCTURE_H
-#include "ContourStructure/contourstructure.h"
+
+#include "contourstructure.h"
 
 class NaiveContourStructure : public ContourStructure {
 public:
-    NaiveContourStructure(int n=0, int m=0) ; // n,m is the size of matrix
-    int Update(int x1, int y1, int x2, int y2, int add) ;
-    // Need Implemention
+    NaiveContourStructure(int n=0, int m=0); // n,m is the size of matrix
+    ~NaiveContourStructure();
+    int Update(int x1, int y1, int x2, int y2, int add);
+private:
+	int n, m;
+	int *grid;
 };
 
 #endif // NAIVECONTOURSTRUCTURE_H
