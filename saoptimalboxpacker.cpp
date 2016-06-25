d2b5e1c6c97883ac22ff513c3e9e70721fcf7edc
= <=>
2016-06-25 15:15:34 +0100

    the excutable version with random-walk algorithm

---
+++ saoptimalboxpacker.cpp
@@ -0,0 +1,21 @@
+#include "kernel.h"
+#include "saoptimalboxpacker.h"
+
+using namespace std ;
+typedef long long LL ;
+
+int SAOptimalBoxPacker::packBoxes(const std::vector<Box>& box, BoxPackage& answer)
+{
+    BoxPerturber* perturber = PS->getBoxPerturber(box) ;
+    int mn = INF, tp ;
+    BoxPackage temp ;
+    for(int i = 0; i < 100000; i ++)
+    {
+        perturber->perturb(Random(10, 1)/10.0);
+        temp = perturber->getBoxPackage() ;
+        tp = VS->getWeight(temp) ;
+        if(mn > tp)
+            mn = tp, answer = temp ;
+    }
+    return mn ;
+}
