7e2700953515edeec24c5c98b16479478639f733
juked060 <yschung55@hotmail.com>
2016-06-25 15:40:24 +0800

    Added SA concept to packBoxes function

--- saoptimalboxpacker.cpp
+++ saoptimalboxpacker.cpp
@@ -8,16 +8,42 @@ int SAOptimalBoxPacker::packBoxes(const std::vector<Box>& box, BoxPackage& answe
 {
     ofstream os("Debug.txt") ;
     BoxPerturber* perturber = PS->getBoxPerturber(box) ;
-    int mn = INF, tp ;
-    BoxPackage temp ;
-    for(int i = 0; i < 100000; i ++)
+    
+    int bestCost = INF, tCost ; // bestCost storesthe best cost of the packging, tCost is the temporary
+    BoxPackage bestPackage, tPackage; // bestPackage storesthe best cost of the packging, tPackage is the temporary
+    
+    ///
+    double T = 1.0; //starting temperature
+    double T_min = 0.000001;  //minimum temperature at which process stops
+    double alpha = 0.9 //increment the temperature
+    ///
+    
+    while (T > T_min)
     {
-        perturber->perturb(Random(10, 1)/10.0);
-        temp = perturber->getBoxPackage() ;
-        tp = VS->getWeight(temp) ;
-        os << mn << " " << tp << endl ;
-        if(mn > tp)
-            mn = tp, answer = temp ;
+        for(int i = 0; i < 100; i ++)
+        {
+            perturber->perturb(Random(10, 1)/10.0);
+            tPackage = perturber->getBoxPackage() ;
+            tCost = VS->getWeight(tPackage) ;
+            os << bestCost << " " << tCost << endl ;
+            
+            if(tCost < bestCost)
+            {    
+                bestCost = tCost; 
+                bestPackage = tPackage;
+                answer = bestPackage; 
+            }
+            else
+            {
+                if(Config->accProb(bestCost, tCost, T) > Random(1, 0))
+                {   
+                    bestCost = tCost; 
+                    bestPackage = tPackage;
+                }   
+            }
+            
+        }
+        T *= alpha;
     }
-    return mn ;
+    return bestCost;
 }
